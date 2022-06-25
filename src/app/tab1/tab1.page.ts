093e0969b4bbcafbe136634f42b8c29e935d7771
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2022-06-25 10:51:25 -0300

    :zap: Add alert button (25/06/2022)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'Deseja Favoritar este filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel', // não mexer
+          handler: (blah) => { // o que vai ser acionado quando o botão for pressionado
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Confirmar',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
