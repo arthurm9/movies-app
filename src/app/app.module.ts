39ec026ff77cfd54d0b2e794a88035d6f36bac34
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2022-06-30 21:22:07 -0300

    Add pipe line (30/06/2022)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { DadosService } from './services/dados.service';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -11,13 +11,18 @@ import { AppComponent } from './app.component';
 // importação http para url de busca de filmes
 import { HttpClientModule } from '@angular/common/http';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   // sempre que um app é iniciado, ele carrega os providers, consequentemente os 'DadosService'
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  DadosService
+  DadosService, { provide: LOCALE_ID, useValue: 'pt-BR'},
   ],
   bootstrap: [AppComponent],
 })
